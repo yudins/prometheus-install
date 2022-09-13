e7b07200ad3561e583739e42502425bb85d4cac9
yudins <foontomas@gmail.com>
2022-09-13 08:47:48 +0300

    Update node.sh
    Add systemd conf & service status

--- node.sh
+++ node.sh
@@ -13,11 +13,26 @@ sudo cp node_exporter-${VERSION}.linux-amd64/node_exporter /usr/local/bin/
 sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
 
 # systemd
-cat ./node/node_exporter.service | sudo tee /etc/systemd/system/node_exporter.service
+cat > /etc/systemd/system/node_exporter.service <<- EOF
+[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=multi-user.target
+EOF
 
 sudo systemctl daemon-reload
 sudo systemctl enable node_exporter
 sudo systemctl start node_exporter
+sudo systemctl status node_exporter
 
 # Installation cleanup
 rm node_exporter-${VERSION}.linux-amd64.tar.gz
